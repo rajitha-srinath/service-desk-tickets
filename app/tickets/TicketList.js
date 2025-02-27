14d37a263416efc20b1f1f48abd20df8dbdba255
rajithas <s.rajitha@directfn.com>
2025-02-27 10:26:41 +0530

    generateStaticParams

--- app/tickets/TicketList.js
+++ app/tickets/TicketList.js
@@ -1,31 +1,34 @@
-async function getTickets () {
-    const res = await fetch('http://localhost:4000/tickets', {
-        next: {
-            revalidate: 0
-        }
-    });
+import Link from 'next/link';
 
-    return await(res.json());
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0,
+    },
+  });
 
+  return await res.json();
 }
 
-export default async function TicketList () {
-    const tickets = await getTickets();
+export default async function TicketList() {
+  const tickets = await getTickets();
 
-    return (
-        <>
-            {tickets.map((ticket) => (
-                <div key={ticket.id} className="card my-5">
-                    <h3>{ticket.title}</h3>
-                    <p>{ticket.body.slice(0, 100)}...</p>
-                    <div className={`pill ${ticket.priority}`}>
-                        {ticket.priority} priority
-                    </div>
-                </div>
-            ))}
-            {tickets.length === 0 && (
-                <p className="text-center">There are no open Tickets</p>
-            )}
-        </>
-    )
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <Link href={`/tickets/${ticket.id}`}>
+          <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 100)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </div>
+        </Link>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open Tickets</p>
+      )}
+    </>
+  );
 }
